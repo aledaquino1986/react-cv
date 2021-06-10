0933d940ba3335aa57c62a281d00af348da6419f
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-06-10 15:32:17 -0300

    fix typos and change Languages heading

--- src/components/sections/skills/Skills.js
+++ src/components/sections/skills/Skills.js
@@ -7,7 +7,11 @@ function Skills({ languages, heading = "Languages", rightSide }) {
     <section
       className={rightSide ? `"right-side__skills"` : "left-side__languages"}
     >
-      {rightSide ? <h2>{heading}</h2> : <h3>{heading}</h3>}
+      {rightSide ? (
+        <h2>{heading}</h2>
+      ) : (
+        <h3 className="text3-heading">{heading}</h3>
+      )}
 
       <ul className={rightSide ? `"right-side__ul"` : "left-side__ul"}>
         {languages.map(({ language, level, id }) => {
